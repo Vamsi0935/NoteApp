1c4c75f78c2e87f11be93f3b49249d8774c8a294
Vamsikrishna <141436747+Vamsi0935@users.noreply.github.com>
2024-08-07 14:16:03 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["https://note-app-frontend-alpha.vercel.app"],
+    origin: ["http://localhost:3000"],
     methods: ["GET","POST","PUT","DELETE"],
     credentials: true,
   })
